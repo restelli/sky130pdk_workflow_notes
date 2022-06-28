4b9eb0c49f528743f2dfb16b59c210172dbe2e68
restelli <arestell@.umd.edu>
2022-06-28 14:56:53 -0400

    Fixed side effect

--- scripts/install.bat
+++ scripts/install.bat
@@ -3,9 +3,14 @@ curl.exe -o start_x_temp.bat https://raw.githubusercontent.com/hpretl/iic-osic-t
 
 SET DOCKER_TAG=latest
 SET DOCKER_IMAGE=skywater_dev_image
-:: Please note that the following line is a hack to add a name tag to the container
-SET DOCKER_USER=--name skywater_dev local
+SET DOCKER_USER=local
 
 docker build https://github.com/restelli/sky130pdk_workflow_notes.git#main -t %DOCKER_USER%/%DOCKER_IMAGE%
+
+:: Please note that the following line is a hack to add a name tag to the container
+SET DOCKER_USER=--name skywater_dev local
 .\start_x_temp.bat
+:: Undouing the hack to avoid side effects
+SET DOCKER_USER=local
+
 rm .\start_x_temp.bat
